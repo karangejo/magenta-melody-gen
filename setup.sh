5bee374788f7b994237a958d25cdd11b64f36a43
karang <gabriel.g.gras@gmail.com>
2020-02-03 15:12:29 +0800

    first commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# run these commands to set up the environment
+sudo apt-get install build-essential libasound2-dev libjack-dev portaudio19-dev
+virtualenv magenta
+source magenta/bin/activate
+pip3 install magenta
+
+# then run some of the commands in sampleCommands.txt
